0aa97d26efb6eee751eba08d1774ebea86af5a3a
PranavPookuth <pranavpookuth@gmail.com>
2024-12-11 17:21:50 +0530

    first commit

--- donor/models.py
+++ donor/models.py
@@ -131,7 +131,6 @@ class BloodDonationSchedule(models.Model):
     class Meta:
         ordering = ['date']  # Ensures that schedules are ordered by date
 
-
 #User Registering For Blood Donation Camp
 class BloodDonationRegistration(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
