3d9056568df5aa83f4dbcf97152c4cf3b4a7e961
PranavPookuth <pranavpookuth@gmail.com>
2024-12-14 16:24:31 +0530

    first commit

--- donor/serializers.py
+++ donor/serializers.py
@@ -39,6 +39,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("A user with this username already exists.")
         return value
 
+
+
+
     def validate_blood_type(self, value):
         allowed_blood_types = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
         if value not in allowed_blood_types:
