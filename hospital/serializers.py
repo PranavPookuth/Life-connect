b338cf8fcfc80e70493adf6e9bee571de22e1467
PranavPookuth <pranavpookuth@gmail.com>
2025-01-09 23:10:59 +0530

    Remove lifeconnect submodule

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -39,7 +39,6 @@ class HospitalRegisterSerializer(serializers.ModelSerializer):
         )
         return hospital
 
-
 class HospitalOTPVerifySerializer(serializers.Serializer):
     email = serializers.EmailField()
     otp = serializers.CharField(max_length=6)
